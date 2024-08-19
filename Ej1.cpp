ccddd42f55e84047bcd36d70dfc7a30a5ccba86f
magdalenagomezf <2302577@ucc.edu.ar>
2024-08-19 11:36:31 -0300

    ej 1y 2

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,47 @@
+/*Ejercicio N°1
+Implementar una función llamada “printLista” que reciba una lista y una posición “p” e imprima
+los elementos de esa lista a partir de la posición “p”. No debe modificarse la lista original. Ej:
+listaOriginal: 1->2->3->4->5->6->7->8->9->10 con p=5 Por pantalla se verá: 5->6->7->8->9->10
+*/
+
 #include <iostream>
+#include "Lista/Lista.h"
 using namespace std;
+
+void printLista(Lista<int> &lista, int p){
+    int tamanio = lista.getTamanio();
+
+    if ( p >= tamanio || p < 0){
+        cout << "La posicion " << p << " esta fuera de los limites de la lista" << endl;
+        return;
+    }
+
+    for (int i=p ; i<tamanio ; i++){
+        cout << lista.getDato(i) << " -> ";
+    }
+    cout << "Null" << endl;
+}
 
 int main() {
 
     cout << "Ejercicio N° 1" << endl;
+
+    Lista <int> Lista1;
+    int n, d, p;
+
+    cout << "Ingrese el tamnio de la lista" << endl;
+    cin >> n;
+
+    for (int i=0 ; i<n ; i++){
+        cout << "Ingrese el valor de la lista en la posicion: " << i << endl;
+        cin >> d;
+        Lista1.insertarUltimo(d);
+    }
+
+    cout << "Ingrese la posicion desde donde desea imprimir la lista1: " << endl;
+    cin >> p;
+
+    printLista(Lista1, p);
 
     return 0;
 }
--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
+#include "Lista/Lista.h"
+//Implementar una función que recibe una lista de números enteros y un número entero “n” y
+//que modifique la lista borrando todos los elementos de la lista que tengan el número “n”.
+//Imprimir la lista antes de hacer el llamado a la función y luego del llamado a la función para
+//mostrar que la lista original cambió. ej: lista: 1->2->3->4->5->3->7->8->3->10 con n=3 debe
+//pasar con la función a 1->2->4->5->7->8.
 
+void borrarnumero(Lista<int> &Lista1,int &pos, int n){
+    for(int i=0;i<pos;i++){
+        if (Lista1.getDato(i)==n){
+            Lista1.remover(i);
+            pos=pos-1;
+        }
+    }
+}
 int main() {
 
+    Lista<int>Lista1;
     cout << "Ejercicio N° 2" << endl;
+    int n,d,l;
+    cout << "Ingrese el tamnio de la lista" << endl;
+    cin >> l;
 
+    for (int i=0 ; i<l ; i++){
+        cout << "Ingrese el valor de la lista en la posicion: " << i << endl;
+        cin >> d;
+        Lista1.insertarUltimo(d);
+    }
+
+    cout << "Ingrese el numero que desea borrar: " << endl;
+    cin >> n;
+
+  Lista1.print();
+
+    borrarnumero(Lista1,l,n);
+
+Lista1.print();
     return 0;
 }
